076f3041f848063060e8e8edd53a0585942989d9
mukeshkumar168 <117968880+mukeshkumar168@users.noreply.github.com>
2025-06-24 11:00:07 +0530

    Delete Desktop/Python_Training directory

--- Desktop/Python_Training/Day1/for_loop.py
+++
@@ -1,8 +0,0 @@
-def count_of_string(str):
-    count = 0
-    for char in str:
-        count+= 1
-    return count
-
-str = input("Enter a string: ")
-print(count_of_string(str))
--- Desktop/Python_Training/test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-# string formatting
-a = "Hello"
-b = "World"
-print("This is first {} and this is second {}".format(a, b))
-
-today = datetime.date.today()
-print(today)
